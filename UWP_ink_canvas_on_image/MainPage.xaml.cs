cb0ccbb3ec14008ee7071406f0ed138391a90e28
yeomyeong.woo <wymstar@naver.com>
2017-02-09 14:16:57 +0900

    이미지 저장기능 및 화면 리사이즈 대응

--- UWP_ink_canvas_on_image/MainPage.xaml.cs
+++ UWP_ink_canvas_on_image/MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Graphics.Canvas;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -15,33 +16,41 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-
-// 빈 페이지 항목 템플릿은 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 에 문서화되어 있습니다.
+using Windows.Media.Capture;
+using System.Numerics;
+using Windows.UI.Input.Inking;
 
 namespace UWP_ink_canvas_on_image
 {
-    /// <summary>
-    /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        private float firstImageWidth;
+        private float firstImageHeight;
+        private int imageWidth;
+        private int imageHeight;
+
         public MainPage()
         {
             this.InitializeComponent();
             ink.InkPresenter.InputDeviceTypes = CoreInputDeviceTypes.Mouse | CoreInputDeviceTypes.Touch;
+            firstImageWidth = (float)image.ActualWidth;
+            firstImageHeight = (float)image.ActualHeight;
+            imageWidth = (int)image.ActualWidth;
+            imageHeight = (int)image.ActualHeight;
         }
 
         private async void BtnSave_Click(object sender, RoutedEventArgs e)
         {
             // grab output file
-
-            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFolder storageFolder = KnownFolders.SavedPictures;
             var file = await storageFolder.CreateFileAsync("output.jpg", CreationCollisionOption.ReplaceExisting);
 
             CanvasDevice device = new CanvasDevice();
             device = CanvasDevice.GetSharedDevice();
-            CanvasRenderTarget renderTarget = new CanvasRenderTarget(device, (int)ink.ActualWidth, (int)ink.ActualHeight, 96);
+            CanvasRenderTarget renderTarget = new CanvasRenderTarget(device, imageWidth, imageHeight, 96);
+            
 
             // grab your input file from Assets folder
             StorageFolder appInstalledFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
@@ -51,8 +60,9 @@ namespace UWP_ink_canvas_on_image
             {
                 ds.Clear(Colors.White);
                 var image = await CanvasBitmap.LoadAsync(device, inputFile.Path);
+                
                 // draw your image first
-                ds.DrawImage(image);
+                ds.DrawImage(image, new Rect(new Point(0,0), new Point(imageWidth, imageHeight))); //Rect
                 // then draw contents of your ink canvas over it
                 ds.DrawInk(ink.InkPresenter.StrokeContainer.GetStrokes());
             }
@@ -62,6 +72,40 @@ namespace UWP_ink_canvas_on_image
             {
                 await renderTarget.SaveAsync(fileStream, CanvasBitmapFileFormat.Jpeg, 1f);
             }
+        }
+
+        private void App_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            setFirstImageSize();
+            imageWidth = (int)image.ActualWidth;
+            imageHeight = (int)image.ActualHeight;
+
+            if (firstImageWidth > 0 && firstImageHeight > 0)
+            {
+                
+                var widthScale = imageWidth / firstImageWidth;
+                var heightScale = imageHeight / firstImageHeight;
+                
+                Debug.WriteLine("image size : " + imageWidth + " x " + imageHeight);
+                Debug.WriteLine("resize scale : " + widthScale + " x " + heightScale);
+                if (widthScale != 1)
+                {
+                    foreach (var stroke in ink.InkPresenter.StrokeContainer.GetStrokes())
+                    {
+                        stroke.PointTransform = Matrix3x2.CreateScale(widthScale, heightScale);
+                        Debug.WriteLine(stroke.PointTransform.ToString());
+                    }
+                }
+            }
+        }
+
+        private void setFirstImageSize ()
+        {
+            if (firstImageWidth == 0)
+                firstImageWidth = (int)image.ActualWidth;
+            if (firstImageHeight == 0)
+                firstImageHeight = (int)image.ActualHeight;
+
         }
     }
 }
